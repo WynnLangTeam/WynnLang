ef39d15a5bb1d0193fe1f4a62174b83b0b60abf4
ArtFect <fiwfeo@gmail.com>
2020-11-15 20:43:33 +0700

    Preparing to moving window name translation from packets to events

--- src/main/java/ru/artfect/wynnlang/hooklib/WynnLangHookLoader.java
+++ src/main/java/ru/artfect/wynnlang/hooklib/WynnLangHookLoader.java
@@ -14,7 +14,8 @@ public class WynnLangHookLoader extends HookLoader {
 
     @Override
     public void registerHooks() {
-        //регистрируем класс, где есть методы с аннотацией @Hook
+        //регистрируем классы, где есть методы с аннотацией @Hook
         registerHookContainer("ru.artfect.wynnlang.hooklib.hooks.EventHooks");
+        registerHookContainer("ru.artfect.wynnlang.hooklib.hooks.WindowName");
     }
 }
